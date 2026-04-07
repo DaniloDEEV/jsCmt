1dca81393f1611b9352e062f15d5d26186b3e0cf
danilo cordeiro <200597866+DaniloDEEV@users.noreply.github.com>
2026-04-07 19:17:29 -0300

    new exercises
    saving new workouts that end in javascript and html

---
+++ TreinosPesoal/Treino-JS-e-HTML-/10-Fazer- js-da-pagiona/script.js
@@ -0,0 +1,16 @@
+
+let btn = document.getElementById("btn")
+
+btn.addEventListener("click", ()=>{
+let name = document.getElementById  ("nome").value
+let plate = document.getElementById("placa").value
+let transporter = document.getElementById("transportadora").value
+let list = document.getElementById("lista")
+
+    if(name.length == 0 || plate.length == 0 ||  transporter.length == 0){
+    alert("erros")
+   }else{
+    
+    
+}
+})
