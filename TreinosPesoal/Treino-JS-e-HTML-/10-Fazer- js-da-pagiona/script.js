c2c42206a2187730a3c262ad472a57e7969a9013
danilo cordeiro <200597866+DaniloDEEV@users.noreply.github.com>
2026-04-10 18:13:13 -0300

    atualização do siste
    a pagina que to treinando java script

--- TreinosPesoal/Treino-JS-e-HTML-/10-Fazer- js-da-pagiona/script.js
+++ TreinosPesoal/Treino-JS-e-HTML-/10-Fazer- js-da-pagiona/script.js
@@ -1,16 +1,23 @@
+    let btn = document.getElementById("btn")
 
-let btn = document.getElementById("btn")
+    btn.addEventListener("click", ()=>{
+    let name = document.getElementById  ("nome") 
+    let plate = document.getElementById("placa")
+    let transporter = document.getElementById("transportadora")
+    let list = document.getElementById("lista")
 
-btn.addEventListener("click", ()=>{
-let name = document.getElementById  ("nome").value
-let plate = document.getElementById("placa").value
-let transporter = document.getElementById("transportadora").value
-let list = document.getElementById("lista")
-
-    if(name.length == 0 || plate.length == 0 ||  transporter.length == 0){
-    alert("erros")
-   }else{
-    
-    
-}
-})
+    if(name.value.trim() === "" || plate.value === 0 ||
+      transporter.value.trim() === 0){
+        let inputs = document.querySelectorAll(".inputs")
+        inputs.forEach((valor)=>{
+         if(valor.value.trim() === ""){
+         valor.style.backgroundColor = "red"
+         }else{
+         valor.style.backgroundColor = "green"
+          }
+           })}else{
+            let li = document.createElement("li")
+            li.textContent = `Nome: ${name.value} | Placa: ${plate.value} | Transportador: ${transporter.value}`
+            list.appendChild(li)
+                     }
+                      })
